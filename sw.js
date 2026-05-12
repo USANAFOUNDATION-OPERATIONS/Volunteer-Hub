4b7c3801220267168e643ef7389fa80e613d09a8
USANAFOUNDATION-OPERATIONS <stephen.cook@usanainc.com>
2026-05-12 11:16:05 -0600

    Add PWA manifest and service worker for Android fullscreen install

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE='kids-eat-cc-v1';
+const SHELL=[
+  '/Volunteer-Hub/command_center.html',
+  '/Volunteer-Hub/manifest.json'
+];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    fetch(e.request).catch(()=>caches.match(e.request))
+  );
+});
